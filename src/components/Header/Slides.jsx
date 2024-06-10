99ec69fc99099f8b38d35aaa3295cda21da7b607
Pricila Badilla <pricila.badilla@west-ingenieria.cl>
2024-06-10 15:43:46 -0400

    style: avance header

--- src/components/Header/Slides.jsx
+++ src/components/Header/Slides.jsx
@@ -1,11 +1,14 @@
 import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 import './style.css';
 
 
 const slides = [
-    { image: '/header1.webp', textClass: 'text-[#75B7D1] font-radwave title'},
+    { image: "bg-[url('/header1.webp')]", textClass: 'text-[#75B7D1] font-radwave title'},
+    { image: "bg-[url('/header2.webp')]", textClass: 'text-[#75B7D1] font-radwave title'},
+    
 
 ];
 
@@ -20,11 +23,8 @@ const Slides = () => {
                 <Swiper
                     effect={'coverflow'}
                     spaceBetween={0}
-                    grabCursor={true}
                     centeredSlides={true}
                     centeredSlidesBounds={true}
-                    loop={false}
-                    initialSlide={3}
                     slideToClickedSlide={true}
                     slidesPerView={1}
                     onSwiper={(swiper) => {
@@ -33,23 +33,27 @@ const Slides = () => {
                 >
                     {slides.map((slide, index) => (
                         <SwiperSlide key={index}>
-                            <div className="relative container">
-                                <img className="object-cover" src={slide.image} alt={`slide_${index+1}`} />
-                                <div className="absolute top-[50%] left-0">
-                                    <p className="text-white pt-4">Rincón donde la tierra misma te abraza y te renueva.</p>
-                                    <div>
-                                        <h2 className="font-radwave text-4xl text-transparent stroke-1px-white title">Naturaleza simplemente</h2>
+                            <div className={"relative h-screen w-screen container-image-mobile lg:container-image " + slide.image}>
+                                <div className="absolute top-[25%] lg:top-[40%] left-0 z-10 px-10">
+                                    <p className="text-white pt-4 text-lg uppercase">Rincón donde la tierra misma te abraza y te renueva.</p>
+                                    <div className="">
+                                        <h2 className="font-radwave text-transparent stroke-1px-white title">Naturaleza</h2>
+                                        <h2 className="font-radwave text-transparent stroke-1px-white title">simplemente</h2>
                                         <h2 className={slide.textClass}>poderosa</h2>
                                     </div>
-                                    <button className="bg-white rounded-full px-4 flex text-2xl items-center gap-x-1 py-1">Explora
+                                    <button className="bg-white rounded-full px-6 flex text-xl items-center gap-x-2 py-2 mt-2 uppercase">Explora
                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M4 13L4 11L16 11L10.5 5.50004L11.92 4.08004L19.84 12L11.92 19.92L10.5 18.5L16 13L4 13Z" fill="black"/>
                                         </svg>
                                     </button>
                                 </div>
+                                <div className="absolute top-1/2 right-0 transform -translate-y-1/2 px-10 z-10 hidden lg:visible">
+                                    <img className="max-w-[40vw]" src="./map.png" alt="Mapa" />
+                                </div>
                             </div>
                         </SwiperSlide>
                     ))}
+
                 </Swiper>
             </div >
         </>
