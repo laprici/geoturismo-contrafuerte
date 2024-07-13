f41dcbb0c0867ff76351d8d4ed78bf89d1eccda2
Prici <pricilabadilla@gmail.com>
2024-07-13 16:03:02 -0400

    correccion mobile

--- src/components/Header/Slides.jsx
+++ src/components/Header/Slides.jsx
@@ -52,7 +52,7 @@ const Slides = () => {
                                 </button>
                             </div>
                             <div className="absolute top-10 lg:top-1/2 right-0 transform translate-y-1/4 lg:-translate-y-1/2 px-geo z-10">
-                                <img className="max-w-[40vw] min-w-[200px] map-img-h md:w-[500px] 2xl:w-[600px]"  src="./map.png" alt="Mapa" />
+                                <img className="max-w-[30vw] min-w-[200px] map-img-h md:w-[500px] 2xl:w-[600px]"  src="./map.png" alt="Mapa" />
                             </div>
                         </div>
                     </SwiperSlide>
